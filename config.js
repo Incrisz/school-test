f77d6c464cc0cffdff49144392acb27b2ca2e025
incrisz <incrisz4luv@gmail.com>
2025-10-23 16:58:56 +0100

    const backend_url = 'https://api.bmp.com.ng';

--- config.js
+++ config.js
@@ -1,4 +1,1 @@
-// Dynamically determine backend URL based on environment
-const backend_url = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' 
-    ? 'http://127.0.0.1:8000' 
-    : 'https://api.bmp.com.ng';
+const backend_url = 'https://api.bmp.com.ng';
